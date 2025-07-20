41fc068106b080971a18bd644912b60891919995
Nilo18 <nikalongurashvili@gmail.com>
2025-07-20 19:51:05 +0400

    Finished slider logic, only responsive design left

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -14,27 +14,59 @@ export class SliderComponent {
     },
     {
       img: 'assets/slide2.png',
-      text: 'ექსპერტები პროგრამირებაში, დიზაინში, ბიზნესში და მრავალ თანამედროვე პროფესიაში.'
-    }
-    
+      text: 'ექსპერტები პროგრამირებაში, დიზაინში, ბიზნესში და მრავალ თანამედროვე სპეციალობაში.'
+    },
+    {
+      img: 'assets/slide3.png',
+      text: 'სამი მარტივი ნაბიჯი: მოძებნე, დაჯავშნე, მიიღე სარგებლიანი რჩევა.'
+    },
+    {
+      img: 'assets/slide4.jpg',
+      text: 'განავითარე უნარები და მიიღე რჩევები, რომლებიც მართლა მუშაობს.'
+    },
   ]
 
   constructor(@Inject(PLATFORM_ID) private platformid: Object) {}
 
   currentIndex: number = 0;
   intervalId: any
+  nextWasClicked: boolean = false; // This flag will be used to avoid auto moving and next button running at the same time
+  btnWasClicked: boolean = false // This flag will be used to avoid spam clicking next or prev buttons
+  i: number = 1;
 
   next() {
-    this.currentIndex = (this.currentIndex === this.slides.length - 1)? 0 : this.currentIndex + 1
+    if (!this.btnWasClicked) {
+      this.currentIndex = (this.currentIndex === this.slides.length - 1)? 0 : this.currentIndex + 1;
+      this.nextWasClicked = true;
+      this.btnWasClicked = true;
+
+      setTimeout(() => {
+        this.btnWasClicked = false; // Reset the flag after 1 second
+      }, 1000)
+    }
   }
  
+
   prev() {
-    this.currentIndex = (this.currentIndex === 0)? this.slides.length - 1 : this.currentIndex - 1;
+    if (!this.btnWasClicked) {
+      this.currentIndex = (this.currentIndex === 0)? this.slides.length - 1 : this.currentIndex - 1;
+      this.btnWasClicked = true;
+
+      setTimeout(() => {
+        this.btnWasClicked = false;
+      }, 1000)
+    }
+      
   }
 
   autoMoveSlide() {
     this.intervalId = setInterval(() => {
-      this.next();
+      // Check if next button was clicked
+      if (!this.nextWasClicked) {
+        this.next(); // If it wasn't move to the next slide automatically
+      }
+
+      this.nextWasClicked = false; // Reset the nextWasClicked flag at the end of each auto move
     }, 3000)
   }
 
